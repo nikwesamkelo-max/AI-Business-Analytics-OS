6b39d76f9aea6eb167b168e2c2e14f395af0cf6e
nikwesamkelo-max <nikwesamkelo@gmail.com>
2026-06-27 16:34:33 +0200

    Implement trip addition and viewing in main
    Added functionality to add and view trips in the main function.

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
-from database import create_tables
+from database import create_tables, add_trip, view_trips
+
 
 def main():
+
     create_tables()
-    print("🚖 AI Business Analytics OS Started")
+
+    add_trip(
+        customer_name="John",
+        phone="0812345678",
+        pickup="Pinetown",
+        destination="King Shaka Airport",
+        vehicle="7 Seater",
+        driver="Ngwane",
+        fare=550,
+        fuel_cost=120,
+        payment_method="Cash",
+        trip_date="2026-06-27"
+    )
+
+    print("\n------ ALL TRIPS ------")
+
+    trips = view_trips()
+
+    for trip in trips:
+        print(trip)
+
 
 if __name__ == "__main__":
     main()
