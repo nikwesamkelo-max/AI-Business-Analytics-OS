da03c029f51bfb7c40ebce01ebcc273555186d7d
nikwesamkelo-max <nikwesamkelo@gmail.com>
2026-06-28 11:09:29 +0200

    Refactor app.py to include main function and add trip

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from database import create_tables, add_trip
 from analytics import (
     total_trips,
     total_revenue,
@@ -8,12 +9,33 @@ from analytics import (
     most_popular_route
 )
 
-print("\n========== AI BUSINESS DASHBOARD ==========")
-print(f"🚖 Total Trips      : {total_trips()}")
-print(f"💰 Revenue          : R{total_revenue():.2f}")
-print(f"⛽ Fuel Cost        : R{total_fuel_cost():.2f}")
-print(f"📈 Profit           : R{total_profit():.2f}")
-print(f"🏆 Highest Fare     : R{highest_fare():.2f}")
-print(f"💵 Average Fare     : R{average_fare():.2f}")
-print(f"🛣️ Popular Route    : {most_popular_route()}")
-print("===========================================")
+def main():
+
+    create_tables()
+
+    add_trip(
+        customer_name="John",
+        phone="0812345678",
+        pickup="Pinetown",
+        destination="King Shaka Airport",
+        vehicle="7 Seater",
+        driver="Ngwane",
+        fare=550,
+        fuel_cost=120,
+        payment_method="Cash",
+        trip_date="2026-06-27"
+    )
+
+    print("\n========== AI BUSINESS DASHBOARD ==========")
+    print(f"🚖 Total Trips      : {total_trips()}")
+    print(f"💰 Revenue          : R{total_revenue():.2f}")
+    print(f"⛽ Fuel Cost        : R{total_fuel_cost():.2f}")
+    print(f"📈 Profit           : R{total_profit():.2f}")
+    print(f"🏆 Highest Fare     : R{highest_fare():.2f}")
+    print(f"💵 Average Fare     : R{average_fare():.2f}")
+    print(f"🛣️ Popular Route    : {most_popular_route()}")
+    print("===========================================")
+
+
+if __name__ == "__main__":
+    main()
